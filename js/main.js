59a85a1de0419d9c1a4c36845289b1d123b1ac8b
Augusto Martinez <acm_asd@hotmail.com>
2019-07-31 14:49:42 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,55 @@
+let btnOpen = document.getElementById("open-mod");
+  let btnHide = document.getElementById("btn-close");
+  
+  btnOpen.addEventListener("click", showModal, false);
+  btnHide.addEventListener("click", hideModal, false);
+  
+  function showModal(evt) {
+    let id = evt.currentTarget.dataset.target;    
+    let root = document.getElementsByTagName("body")[0];
+    let tgt = document.getElementById(id);    
+    let modalclass = "active";    
+    let rootclass = "modal-open";
+    
+    addClass(rootclass, root);
+    addClass(modalclass, tgt);    
+  }
+  
+  function hideModal(evt) {
+      let id = evt.currentTarget.dataset.target;
+      let root = document.getElementsByTagName("body")[0];
+      let tgt = document.getElementById(id);      
+      let modalclass = "active";
+      let rootclass = "modal-open";
+      
+      removeClass(rootclass, root);
+      removeClass(modalclass, tgt);           
+  }
+  
+  function addClass(name, elem) {
+    if (elem.classList) {
+      elem.classList.add(name);
+    }
+    else {
+      let arr = elem.className.split(" ");
+      let i = arr.indexOf(name);
+      if (i == -1) {
+        arr.push(name)
+        elem.className = arr.join(" ");
+      }
+    }
+  }
+  
+  function removeClass(name, elem) {
+    if (elem.classList) {
+      elem.classList.remove(name);
+    }
+    else {
+      let arr = elem.className.split(" ");
+      let i = arr.indexOf(name);
+      if (i >= 0) {
+        arr.splice(i, 1);
+        elem.className = arr.join(" ");
+      }
+    }
+  }
